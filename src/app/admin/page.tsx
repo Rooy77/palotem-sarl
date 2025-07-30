93058037305bfacb2ce2a03bdc71d94ba3e14523
Daniel Rooy <157033610+Rooy77@users.noreply.github.com>
2025-07-30 14:29:17 +0100

    erreur deployement

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,7 +5,7 @@ import CreatePost from "@/components/creatPost"
 export default function AdminPage() {
   return (
     <main className="max-w-3xl mx-auto p-8">
-        <h1 className="text-4xl mb-6">Espace Admin</h1>
+        <h1 className="text-4xl mb-8 px-4">Espace Admin</h1>
         <div className="grid grid-cols-2 gap-6">
              <CreatePost />
         </div>
