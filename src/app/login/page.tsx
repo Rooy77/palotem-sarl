45a8d384fc509f41245f00e28da9a6be37e2900f
Daniel Rooy <157033610+Rooy77@users.noreply.github.com>
2025-08-02 12:16:09 +0100

    push main

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,8 @@
 
 export default function SkeletonCard() {
   return (
-    <div className="rounded-xl bg-gray-200 animate-pulse h-64 w-full"></div>
+    <div className="rounded-xl bg-gray-200 animate-pulse h-64 w-full">
+        <div className="h-32 bg-gray-300 rounded-t-xl"></div>
+    </div>
   );
 }
